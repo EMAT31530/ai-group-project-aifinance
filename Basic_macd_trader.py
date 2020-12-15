4eb83bdec868280eccc9ddffce68d31fa250eec6
NateBrowne <73693166+NateBrowne@users.noreply.github.com>
2020-12-15 17:17:15 +0000

    Not Much
    Just neatened so I can understand and make an SMA trader

--- Basic_macd_trader.py
+++ Basic_macd_trader.py
@@ -3,12 +3,8 @@ import numpy as np
 # Returns zero_crossings
 def macd_crossing(macd_signal, macd): # identifying where macd shows buy/sell signal
     macd_diff = macd_signal - macd
-
     zero_crossings = np.where(np.diff(np.sign(macd_diff)))[0]
-    zero_crossings = zero_crossings
-
     return zero_crossings
-
 
 # Returns action_index_dict
 def locate_action(zero_crossings, macd, macd_signal): # determines buy and sell positions
@@ -18,12 +14,10 @@ def locate_action(zero_crossings, macd, macd_signal): # determines buy and sell 
             action_index_dict[k] ='sell'
         else:
             action_index_dict[k] = 'buy'
-
     return action_index_dict
 
 # Returns action_index_dict
 def basic_trader(macd, macd_signal): # finds action points for stock
     zero_crossings = macd_crossing(macd_signal, macd)
     action_index_dict = locate_action(zero_crossings, macd, macd_signal)
-
     return action_index_dict
