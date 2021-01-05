4e1e641c023437be6ba92b36cd01ecceee93962b
NateBrowne <73693166+NateBrowne@users.noreply.github.com>
2021-01-05 19:05:29 +0000

    Update K-Means.py

--- Nate's Stuff/K-Means.py
+++ Nate's Stuff/K-Means.py
@@ -20,8 +20,9 @@ today = date.today()
 #takes a stock ticker, a start and end date, then returns training and test data!
 def get_price_data(ticker, start, end):
     raw = yf.download(ticker, start, end).dropna() #grab some data
-    close = pd.DataFrame(raw["Adj Close"]) #we only want closing prices as a dataframe object
-    return close
+    data = pd.DataFrame(raw["Adj Close"]).dropna() #we only want closing prices as a dataframe object
+    data.reset_index(drop=True, inplace=True)
+    return data
 
 def add_MAs(df, ShortPeriod=20, LongPeriod=50):
     df['SMAshort'] = df['Adj Close'].rolling(ShortPeriod).mean()
@@ -33,6 +34,7 @@ def add_MACD(df):
     EMA26 = df['Adj Close'].ewm(span=26).mean()
     df['MACD'] = EMA12 - EMA26
     df['Signal'] = df['MACD'].ewm(span=9).mean()
+    df['Hist'] = df['MACD'] - df['Signal']
     return df
 
 def add_RSI(df, period=14):
@@ -53,14 +55,17 @@ def plot_chart(df):
     ax_closes = fig.add_subplot(grid[:-5, 0:])
     ax_macd = fig.add_subplot(grid[-5:-3, 0:], sharex=ax_closes)
     ax_rsi = fig.add_subplot(grid[-3:-1, 0:], sharex=ax_closes)
-    ax_closes.xaxis_date()
+    #ax_closes.xaxis_date()
     ax_closes.plot(df['Adj Close'], label='Close Price')
     ax_closes.plot(df['SMAshort'], label='Short SMA')
     ax_closes.plot(df['SMAlong'], label='Long SMA')
     ax_closes.legend()
+
     ax_macd.plot(df['MACD'], label='MACD')
     ax_macd.plot(df['Signal'], label='Signal')
+    ax_macd.bar(df.index, df['Hist'])
     ax_macd.legend()
+
     ax_rsi.set_ylabel("(%)")
     ax_rsi.plot(df.index, [70]*len(df.index), label='Overbought')
     ax_rsi.plot(df.index, [30]*len(df.index), label='Oversold')
@@ -85,11 +90,11 @@ def split_df(df):
 
 #START OF PROGRAM
 logging.debug('PROGRAM EXECUTION INITIATED')
-ShortPeriod = 5
-LongPeriod = 20
+ShortPeriod = 10
+LongPeriod = 40
 
 #Get some data
-data = get_price_data('AAPL', '2010-01-01', '2016-01-01')
+data = get_price_data('AAPL', '2010-01-01', '2020-01-01')
 #Add moving Averages:
 data = add_MAs(data, ShortPeriod, LongPeriod)
 #Add MACD:
@@ -97,12 +102,10 @@ data = add_MACD(data)
 #Add RSI:
 data = add_RSI(data)
 
-data['20perchange'] = np.log()
-
-data['Returns'] = np.log(data['Adj Close'] / data['Adj Close'].shift(1))
+data['20perchange'] = np.log(data['Adj Close'] / data['Adj Close'].shift(20))
 data = data.dropna()
 
-logging.debug(data)
+logging.debug(data.tail())
 plot_chart(data)
 
 #Now it is time to split into Training and Test Data:
@@ -129,6 +132,6 @@ logging.debug(test.head())
 # #Test this produces similar calls
 # train2['pred_return'] = model.predict(train2[['SMAsig', 'MACDsig', 'RSIsig']])
 
-logging.debug(train2.tail())
+# logging.debug(train2.tail())
 
 logging.debug('PROGRAM TERMINATED')
