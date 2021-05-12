f10d716aed2cd202d4efe204236fa2908a170f09
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-05-12 19:41:18 +0100

    Final Touches & Comments

--- Shane's Stuff/build_data.py
+++ Shane's Stuff/build_data.py
@@ -1,22 +1,23 @@
 #CNN using graphs as the input to our model
-
-#From Tutorial at http://amunategui.github.io/unconventional-convolutional-networks/
-
+#Build data for model
 import time
 import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas_datareader as pdr
 from sklearn.preprocessing import MinMaxScaler
 from useful_functions import *
 import os, sys
 import yfinance as yf
 
 
+
+
 def build_data(STOCKS,TIME_RANGE,PRICE_RANGE,VALIDTAION_CUTOFF_DATE):
     
-    stock = STOCKS[0]
+    #STOCKS : List of stock Tickers
+    
+    stock = STOCKS[0]#Only using one in the end, initially planned for many stocks
     budget = 1000
     
     # split image horizontally into two sections - top and bottom sections
@@ -28,29 +29,26 @@ def build_data(STOCKS,TIME_RANGE,PRICE_RANGE,VALIDTAION_CUTOFF_DATE):
     x_valid = None
     y_train = []
     y_valid = []
-    for stock in STOCKS:
-        print(stock)
         
         def format_date(stock_data):
+            #Reformat date
             stock_data['Symbol'] = stock
             stock_data['Date'] = stock_data.index
             stock_data['Date'] = pd.to_datetime(stock_data['Date'], infer_datetime_format=True)
             stock_data['Date'] = stock_data['Date'].dt.date
             stock_data = stock_data.reset_index(drop=True)
             return stock_data
-            
-        # build image data for this stock
-        # stock_data = pdr.get_data_google(stock)
     
-        # download dataframe
-        stock_data = yf.download(stock, start="2004-01-01", end="2021-01-01")
+        # download dataframe for stock
+        stock_data = yf.download(stock, start="2010-01-01", end="2021-05-09")
         
-        #Indicators
+        #Indicators we want to base our trading strategy off
         rsi = rsi_calc(stock_data['Close']).fillna(method='bfill')
         macd, macd_signal = macd_calc(stock_data['Close'])[0].fillna(method='bfill'), macd_calc(stock_data['Close'])[1].fillna(method='bfill')
-    
+        
+        #Reformat Date
         stock_data = format_date(stock_data)
-    
+        #Closing Prices
         stock_closes = format_data(stock_data['Close'])  
         stock_opens = format_data(stock_data['Close'])
         
@@ -58,17 +56,16 @@ def build_data(STOCKS,TIME_RANGE,PRICE_RANGE,VALIDTAION_CUTOFF_DATE):
         
     
         for cnt in range(4, len(stock_closes)):
-            if (cnt % 500 == 0): print(cnt)
     
             if (cnt >= TIME_RANGE):
     
-                #Indicators
+                # Scale  Indicators for graph
                 graph_rsi = list(np.round(scale_list(rsi[cnt-TIME_RANGE:cnt], 0, half_scale_size-1),0))
                 graph_macd = list(np.round(scale_list(macd[cnt-TIME_RANGE:cnt], 0, half_scale_size-1),0))
                 graph_macd_signal = list(np.round(scale_list(macd_signal[cnt-TIME_RANGE:cnt], 0, half_scale_size-1),0))
                 
                 
-               # scale both close and close MA toeghertogether
+               # scale closing prices
                 close_data_together = list(np.round(scale_list(list(stock_closes[cnt-TIME_RANGE:cnt]), 0, half_scale_size-1),0))
                 graph_close = close_data_together[0:PRICE_RANGE]
                
@@ -76,43 +73,32 @@ def build_data(STOCKS,TIME_RANGE,PRICE_RANGE,VALIDTAION_CUTOFF_DATE):
                 outcome = None
                 if (cnt < len(stock_closes) -1):
                     outcome = 0
-                    if stock_closes[cnt+1] > stock_closes[cnt]:
+                    if stock_closes[cnt+1] > stock_closes[cnt]: #If stock price goes up --> update
                         outcome = 1
     
-                blank_matrix_close = np.zeros(shape=(half_scale_size, TIME_RANGE))
+                blank_matrix_close = np.zeros(shape=(half_scale_size, TIME_RANGE)) #Canvas for our image
                 x_ind = 0
                 for ma in  graph_close:
-                    blank_matrix_close[int(ma), x_ind] = 2 
+                    blank_matrix_close[int(ma), x_ind] = 2 #Colour closing price pixels
                     x_ind += 1
     
-                # flip x scale dollars so high number is atop, low number at bottom - cosmetic, humans only
+                # If you want to view an image this switches the axes
                 blank_matrix_close = blank_matrix_close[::-1]
                 
                 
-                # store image data into matrix DATA_SIZE*DATA_SIZE
-                blank_matrix_diff = np.zeros(shape=(half_scale_size, TIME_RANGE))
+                blank_matrix_diff = np.zeros(shape=(half_scale_size, TIME_RANGE)) #canvas for second graph
                 x_ind = 0
                 for v,d,e in zip(graph_rsi,graph_macd,graph_macd_signal):
+                    #Colour indicator pixels
                     blank_matrix_diff[int(v), x_ind] = 3
                     blank_matrix_diff[int(d),x_ind] = 4
                     blank_matrix_diff[int(e),x_ind] = 5
                     x_ind += 1
-                # flip x scale so high number is atop, low number at bottom - cosmetic, humans only
+                # If you want to view an image this switches the axes
                 blank_matrix_diff = blank_matrix_diff[::-1]
     
-                blank_matrix = np.vstack([blank_matrix_close, blank_matrix_diff]) 
+                blank_matrix = np.vstack([blank_matrix_close, blank_matrix_diff]) #Put graphs together
                 
-                if cnt==500:
-                    # graphed on matrix
-                    plt.imshow(blank_matrix)
-                    plt.show()
-                    # straight timeseries 
-                    plt.plot(graph_close, color='black')
-                    plt.show()
-    
-                
-    
-    
                 if (outcome == None):
                     # live data
                     if x_live is None:
