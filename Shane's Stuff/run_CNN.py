f527ae16c47d37c0020d3d4898dcede64a1c63e3
Shanehoeb <73693706+Shanehoeb@users.noreply.github.com>
2021-05-12 19:38:38 +0100

    Final changes & Comments

--- Shane's Stuff/run_CNN.py
+++ Shane's Stuff/run_CNN.py
@@ -4,30 +4,20 @@
 
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-import time
-import datetime
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from keras.layers import Activation, Dropout, Flatten, Dense
 from build_data import*
 
-
-
- 
-    
-TIME_RANGE = 30
-PRICE_RANGE = 30
-VALIDTAION_CUTOFF_DATE = datetime.date(2019, 4, 21)
-STOCKS = ['GBPUSD=X']
+TIME_RANGE = 100
+PRICE_RANGE = 100
+VALIDTAION_CUTOFF_DATE = datetime.date(2020, 7, 7)
+STOCKS = ['NFLX']
 batch_size = 1000
 num_classes = 2
 epochs = 10
 budget = 1000
 
-
+#Get all the data
 x_train, y_train, x_valid, y_valid = build_data(STOCKS,TIME_RANGE,PRICE_RANGE,VALIDTAION_CUTOFF_DATE)
 # input image dimensions
 img_rows, img_cols = TIME_RANGE, PRICE_RANGE
@@ -37,109 +27,69 @@ x_train_mod = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_valid = x_valid.reshape(x_valid.shape[0], img_rows, img_cols, 1)
 input_shape = (TIME_RANGE, PRICE_RANGE, 1)
 
+#Convert to float 32
 x_train_mod = x_train_mod.astype('float32')
 x_valid = x_valid.astype('float32')
 
-print('x_train_mod shape:', x_train_mod.shape)
-print('x_valid shape:', x_valid.shape)
- 
+#Convert to keras format
 y_train_mod = keras.utils.to_categorical(y_train, num_classes)
 y_valid_mod = keras.utils.to_categorical(y_valid, num_classes)
 
+#Create Neural Network
 model = Sequential()
 model.add(Conv2D(64, (5, 5), input_shape=input_shape, activation='relu'))
 model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(Conv2D(10, (2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
-
 # Compile model
-opt = keras.optimizers.SGD(lr=0.01, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#Display model summary
 model.summary()
 
-
-######################## testing
-# https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (2, 2)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())  
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('sigmoid'))
-
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
-
-model.summary()
-
-#################################
-
-
+#Fit model to training data
 model.fit(x_train_mod, y_train_mod,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_train_mod, y_train_mod))
- 
-
-score = model.evaluate(x_train_mod, y_train_mod, verbose=0)
-# print('Test loss:', score[0])
-# print('Test accuracy:', score[1])
- 
+		  
+#Get predictions for test set
 predictions_cnn = model.predict(x_valid)
 
-# run an accuracy or auc test
-from sklearn.metrics import roc_curve, auc, accuracy_score
- 
-# balance
-print('Outcome balance %f' % np.mean(y_train_mod[:,1]))
-
-# print('Model accuracy: ', accuracy_score(y_valid_mod[:,1], temp_predictions,'%'))
-fpr, tpr, thresholds = roc_curve(y_valid_mod[:,1], predictions_cnn[:,1])
-roc_auc = auc(fpr, tpr)
-print('AUC: %f' % roc_auc)
-from sklearn.metrics import roc_auc_score
- 
-####################################################################
-# Play around with thresholds to pick the best predictions
-####################################################################
-
-# pick top of class to find best bets 
-actuals = y_valid_mod[:,1]
-preds = predictions_cnn[:,1]
-from sklearn.metrics import accuracy_score
-print ('Accuracy on all data:', accuracy_score(actuals,[1 if x >= 0.5 else 0 for x in preds]))
- 
-threshold = 0.6
-preds = predictions_cnn[:,1][predictions_cnn[:,1] >= threshold]
-actuals = y_valid_mod[:,1][predictions_cnn[:,1] >= threshold]
-from sklearn.metrics import accuracy_score
-print ('Accuracy on higher threshold:', accuracy_score(actuals,[1 if x > 0.5 else 0 for x in preds]))
-print('Returns:',len(actuals))
-
-
 ##Simulate an investment 
-real_predictions = clean_predictions(predictions_cnn[:,1],0.5)
-print(len(real_predictions))
-stock_period = yf.download(STOCKS[0], start="2019-01-01", end="2021-01-01")
-print(len(stock_period['Close']))
+real_predictions = clean_predictions(predictions_cnn[:,1],0.50) #50% threshold
 
-a = profit_calc(budget, correct_action_list(real_predictions),stock_period['Close'])
+#Get data for test period
+stock_period = yf.download(STOCKS[0], start="2020-07-07", end="2021-05-09") 
+
+profit_list = []
+passive_list = []
+higher_confidence = []
+for i in range(1,len(stock_period['Close'])):
+  real_predictions = clean_predictions(predictions_cnn[:i,1],0.5) #All predictions over 50% are converted to buy signals
+  real_predictions1 = clean_predictions(predictions_cnn[:i,1],0.6)#All predictions over 60% are converted to buy signals
+  #Calculate profit for strategies
+  a = profit_calc(budget, correct_action_list(real_predictions),stock_period['Close'][:i]) 
+  b = profit_calc(budget, correct_action_list(real_predictions1),stock_period['Close'][:i])
+  #Append results for plotting
+  profit_list.append(budget + a[0])
+  passive_list.append(budget + a[1])
+  higher_confidence.append(budget + b[0])
+
+import matplotlib.pyplot as plt
+index = []
+for i in range(len(profit_list)): #x axis representing dates
+  index.append(i)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(index, profit_list, color='tab:blue')
+ax.plot(index, passive_list, color='tab:orange')
+ax.plot(index, higher_confidence, color='tab:red')
+plt.show()
+
